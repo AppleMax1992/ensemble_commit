ecd504c9106c696bdc58c10490e6c0b6b1b59d97
Your Name <you@example.com>
2024-07-31 09:43:44 +0800

    base model

--- ensemble_model/ensemble_model_backuo.py
+++ ensemble_model/ensemble_model_backuo.py
@@ -3,6 +3,12 @@ import torch.nn as nn
 from transformers import AdamW, BertModel, BertTokenizer, RobertaModel, RobertaTokenizer
 from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_curve
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from sklearn.manifold import TSNE
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+import seaborn as sns
 # Define base model class
 class BaseModel(nn.Module):
     def __init__(self, transformer_model, tokenizer):
@@ -32,14 +38,15 @@ class StackingModel(nn.Module):
         outputs_codebert = self.base_model2(inputs_codebert)
         combined = torch.cat((outputs_bert, outputs_codebert), dim=1)
         logits = self.classifier(combined)
-        return logits
+        return logits, combined
 
-    def predict(self, sentence1, sentence2):
+    def predict(self, sentence1, sentence2, model_path):
+        self.load_state_dict(torch.load(model_path))
         self.eval()
         with torch.no_grad():
             inputs_bert = self.base_model1.tokenize(sentence1).to(next(self.parameters()).device)
             inputs_codebert = self.base_model2.tokenize(sentence2).to(next(self.parameters()).device)
-            logits = self.forward(inputs_bert, inputs_codebert)
+            logits, _ = self.forward(inputs_bert, inputs_codebert)
             probabilities = torch.softmax(logits, dim=1)
             predicted_class = torch.argmax(probabilities, dim=1).item()
             return predicted_class, probabilities.cpu().numpy()
@@ -53,7 +60,60 @@ class StackingModel(nn.Module):
         plt.legend()
         plt.grid(True)
         plt.show()
+
+    def plot_tsne(self, embeddings, labels):
+        # working code 
+        # tsne = TSNE(n_components=2, random_state=0)
+        # embeddings_np = np.vstack(embeddings, dtype=np.float32)
         
+        # print(embeddings_np.shape)
+        
+        # embeddings_2d = tsne.fit_transform(embeddings_np)
+        
+        # plt.figure(figsize=(10, 8))
+        # scatter = plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=labels, cmap='viridis', alpha=0.5)
+        # plt.colorbar(scatter, label='Labels')
+        # plt.title('t-SNE Visualization of Embeddings')
+        # plt.show()
+
+        
+        tsne = TSNE(n_components=2, random_state=0)
+
+        print("embedding来啦",embeddings[0].shape)
+        # embeddings_cpu = combined.cpu().detach()
+        # embeddings_np = torch.stack(embeddings_cpu).numpy()  # 形状：(600, 8, 768)
+        
+        # 维度变换
+        embeddings_np = np.vstack(embeddings) 
+        embeddings_2d = tsne.fit_transform(embeddings_np)
+        df_tsne = pd.DataFrame(embeddings_2d, columns=['TSNE1', 'TSNE2'])
+        df_tsne['Class Name'] = labels # Add labels column from df_train to df_tsne
+        df_tsne
+        
+        fig, ax = plt.subplots(figsize=(8,6)) # Set figsize
+        sns.set_style('darkgrid', {"grid.color": ".6", "grid.linestyle": ":"})
+        sns.scatterplot(data=df_tsne, x='TSNE1', y='TSNE2', hue='Class Name', palette='hls')
+        sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+        plt.title('Scatter plot of news using t-SNE');
+        plt.xlabel('TSNE1');
+        plt.ylabel('TSNE2');
+        plt.axis('equal')
+
+        # Apply KMeans
+        kmeans_model = KMeans(n_clusters=2, random_state=1, n_init='auto').fit(embeddings_np)
+        labels = kmeans_model.fit_predict(embeddings_np)
+        
+        df_tsne['Cluster'] = labels
+        df_tsne
+        fig, ax = plt.subplots(figsize=(8,6)) # Set figsize
+        sns.set_style('darkgrid', {"grid.color": ".6", "grid.linestyle": ":"})
+        sns.scatterplot(data=df_tsne, x='TSNE1', y='TSNE2', hue='Cluster', palette='magma')
+        sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+        plt.title('Scatter plot of news using KMeans Clustering');
+        plt.xlabel('TSNE1');
+        plt.ylabel('TSNE2');
+        plt.axis('equal')
+
     def trainer(self, train_loader, val_loader, num_epochs=3, learning_rate=2e-5, patience=3):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.to(device)
@@ -67,6 +127,7 @@ class StackingModel(nn.Module):
         for epoch in range(num_epochs):
             self.train()
             total_loss = 0
+            
             for batch in train_loader:
                 inputs_bert, inputs_codebert, labels = batch
                 inputs_bert = {k: v.to(device) for k, v in inputs_bert.items()}
@@ -74,7 +135,7 @@ class StackingModel(nn.Module):
                 labels = labels.to(device)
 
                 optimizer.zero_grad()
-                logits = self(inputs_bert, inputs_codebert)
+                logits, _ = self(inputs_bert, inputs_codebert)
                 loss = criterion(logits, labels)
                 loss.backward()
                 optimizer.step()
@@ -85,7 +146,7 @@ class StackingModel(nn.Module):
             print('=============================train========================')
             print(f'Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss}')
             print('=============================eval========================')
-            val_acc, val_labels, val_probabilities = self.evaluate(val_loader)
+            val_acc, val_labels, val_probabilities, val_embeddings = self.evaluate(val_loader)
             print(f'Epoch {epoch+1}/{num_epochs}, Validation Accuracy: {val_acc}')
             
             if val_acc > best_acc:
@@ -100,9 +161,11 @@ class StackingModel(nn.Module):
                 print('Early stopping')
                 print('best acc是', best_acc)
                 break
+                
         # Plot PR curve for the best model
         self.plot_pr_curve(val_labels, val_probabilities)
-        
+        # Plot t-SNE for the best model
+        self.plot_tsne(val_embeddings, val_labels)
     def evaluate(self, val_loader):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.eval()
@@ -110,25 +173,31 @@ class StackingModel(nn.Module):
         total = 0
         all_labels = []
         all_predictions = []
+        all_probabilities = []
+        all_embeddings = []
         with torch.no_grad():
             for batch in val_loader:
                 inputs_bert, inputs_codebert, labels = batch
                 inputs_bert = {k: v.to(device) for k, v in inputs_bert.items()}
                 inputs_codebert = {k: v.to(device) for k, v in inputs_codebert.items()}
                 labels = labels.to(device)
-                logits = self(inputs_bert, inputs_codebert)
+                logits, embeddings = self(inputs_bert, inputs_codebert)
+                # print("eval embedding",embeddings)
                 _, predicted = torch.max(logits, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
                 all_labels.extend(labels.cpu().numpy())
                 all_predictions.extend(predicted.cpu().numpy())
+                all_probabilities.extend(torch.softmax(logits, dim=1).cpu().numpy()[:, 1]) # probability of the positive class
+                all_embeddings.extend(embeddings.cpu().numpy())
+                
         accuracy = correct / total
-        precision = precision_score(all_labels, all_predictions, average='macro', zero_division=0)
-        recall = recall_score(all_labels, all_predictions, average='macro', zero_division=0)
-        f1 = f1_score(all_labels, all_predictions, average='macro', zero_division=0)
+        precision = precision_score(all_labels, all_predictions, average='weighted', zero_division=0)
+        recall = recall_score(all_labels, all_predictions, average='weighted', zero_division=0)
+        f1 = f1_score(all_labels, all_predictions, average='weighted', zero_division=0)
 
         print(f'Validation Accuracy: {accuracy}')
         print(f'Precision: {precision}')
         print(f'Recall: {recall}')
         print(f'F1-Score: {f1}')
-        return accuracy, all_labels, all_probabilities
+        return accuracy, all_labels, all_probabilities, all_embeddings
